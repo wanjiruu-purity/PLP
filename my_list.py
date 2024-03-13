a489728cdf592f6c363ce57882f7d167ce104235
Purity <118632852+wanjiruu-purity@users.noreply.github.com>
2024-03-13 17:27:57 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,25 @@
+my_list=[]
+print(my_list)
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+my_list.insert(2,15)
+print(my_list)
+
+my_list.extend([50,60,70])
+print(my_list)
+
+del my_list[-1]
+print(my_list)
+
+my_list.sort
+print(my_list)
+
+value = 30
+for index, value in enumerate(my_list):
+    if value == 30:
+        print(value)
